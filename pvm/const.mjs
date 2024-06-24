ec96db71a4a3ecb2ecd6a32228ae7dbeef2d44e0
Luis Espino <66042898+luisespino@users.noreply.github.com>
2024-06-24 00:18:32 -0600

    update

--- pvm/const.mjs
+++ pvm/const.mjs
@@ -14,7 +14,7 @@ export const R_TYPE = 51;
 export const ALIAS = [
     "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", 
     "s0/fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5", 
-    "a6", "a7", "s1", "s2", "s3", "s4", "s5", "s6", "s7", 
+    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", 
     "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6" ];
 
 
