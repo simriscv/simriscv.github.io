8f5d9163461879abfc55937cd6ab9e0c49f82695
Luis Espino <66042898+luisespino@users.noreply.github.com>
2024-07-08 20:42:02 -0600

    update code

--- src/hello.s
+++ src/hello.s
@@ -1,15 +1,15 @@
 .global _start
 
 .data
-    str: .string "Hello, World!\n"
+	str: .string "Hello, World!\n"
 
 .text
 _start:
-    li a0, 1
-    la a1, str
-    li a2, 14
-    li a7, 64
-    ecall
+	li a0, 1
+	la a1, str
+	li a2, 14
+	li a7, 64
+	ecall
 
-    li a7, 93
-    ecall
+	li a7, 93
+	ecall
